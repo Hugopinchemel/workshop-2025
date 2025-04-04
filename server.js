096981d463e7fdc744da8eaed7fedbfe9af70410
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-04-04 15:52:02 +0200

    Update index.html and server.js; modify navbar links and add landing image route

--- server.js
+++ server.js
@@ -23,6 +23,18 @@ app.get('/index-css', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/css/index-css.css'));
 });
 
+//                         _
+//      /\                | |
+//     /  \   ___ ___  ___| |_ ___
+//    / /\ \ / __/ __|/ _ \ __/ __|
+//   / ____ \\__ \__ \  __/ |_\__ \
+//  /_/    \_\___/___/\___|\__|___/
+
+app.get('/landing-img', (req, res) => {
+  res.setHeader('Content-Type', 'img/svg+xml');
+  res.sendFile(path.join(__dirname, 'public/js/index.js'));
+});
+
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${port}/`);
